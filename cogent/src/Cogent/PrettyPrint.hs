da12fc47881d22789fb8ae6fc3ff79eb6eaccd87
Zilin Chen <Zilin.Chen@data61.csiro.au>
2018-07-25 02:52:04 +1000

    wip - some TODOs in the generator
    [skip ci][skip lemma]

--- cogent/src/Cogent/PrettyPrint.hs
+++ cogent/src/Cogent/PrettyPrint.hs
@@ -765,6 +765,8 @@ instance Pretty Assignment where
 instance Pretty a => Pretty (I.IntMap a) where
   pretty = vcat . map (\(k,v) -> pretty k <+> text "|->" <+> pretty v) . I.toList
 
+instance (Pretty k, Pretty v) => Pretty (M.Map k v) where
+  pretty  = vcat . map (\(k,v) -> pretty k <+> text "|->" <+> pretty v) . M.toList
 
 instance Pretty R.RepError where 
   pretty (R.UnknownRepr r ctx) 
